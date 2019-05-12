2c3f36c7a37eba2d9dd9ee4bf4220ee6e41eeb65
rodion <rodion.prosvirnin@bk.ru>
2019-05-12 20:46:57 +0300

    creating folders

---
+++ app/src/main/java/dev/rodni/ru/githubsearch/home/MainActivity.java
@@ -0,0 +1,15 @@
+package dev.rodni.ru.githubsearch.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dev.rodni.ru.githubsearch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
