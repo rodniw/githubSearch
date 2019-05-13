b0b19afae8d41b0d1736ba8b1346bdf04cbbd893
rodniw <47295698+rodniw@users.noreply.github.com>
2019-05-13 22:41:36 +0300

    start adding the github oauth api

---
+++ app/src/main/java/dev/rodni/ru/githubsearch/data/authmodel/AccessToken.java
@@ -0,0 +1,20 @@
+package dev.rodni.ru.githubsearch.data.authmodel;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AccessToken {
+
+    @SerializedName("access_token")
+    private String accessToken;
+
+    @SerializedName("token_type")
+    private String tokenType;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+}
